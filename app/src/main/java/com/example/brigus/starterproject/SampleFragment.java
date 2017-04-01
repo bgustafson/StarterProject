91d780b7ad76e22ac857cf241520639281c8fdc3
bgustafson <bmgustafson@gmail.com>
2017-04-01 10:18:28 -0600

    Initial commit

---
+++ app/src/main/java/com/example/brigus/starterproject/SampleFragment.java
@@ -0,0 +1,28 @@
+package com.example.brigus.starterproject;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SampleFragment extends Fragment {
+
+
+
+
+    public static SampleFragment newInstance() {
+        return new SampleFragment();
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sample, container, false);
+    }
+
+}
